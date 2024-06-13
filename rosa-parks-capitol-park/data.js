e4f7fdc70207b114f4942baca009d482e98c975f
branleeharris <branleeharris@gmail.com>
2024-06-13 18:29:22 -0400

    Rosa / Capitol upload

---
+++ rosa-parks-capitol-park/data.js
@@ -0,0 +1,149 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-rosaparks",
+      "name": "RosaParks",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.17425224387885052,
+        "pitch": 0.22286578628330744,
+        "fov": 0.92862431554155
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8002988232102517,
+          "pitch": -0.07295116612139019,
+          "rotation": 0,
+          "target": "1-westin-em"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.512100532460459,
+          "pitch": -0.08404758671127155,
+          "title": "Parking Lot (Paid)",
+          "text": ""
+        },
+        {
+          "yaw": -0.3213487082505786,
+          "pitch": -0.004360167152212213,
+          "title": "People Mover Entrance",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "1-westin-em",
+      "name": "Westin EM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.3436956065465928,
+        "pitch": 0.1335859432400035,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-capitol-park-saturday-only",
+      "name": "Capitol Park (Saturday Only)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.00165651245832521,
+        "pitch": 0.20992091596249196,
+        "fov": 0.8068572314741396
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.74890227500802,
+          "pitch": 0.34706510374626376,
+          "rotation": 0,
+          "target": "1-westin-em"
+        },
+        {
+          "yaw": 1.7862074392328582,
+          "pitch": -0.7526445517022289,
+          "rotation": 3.141592653589793,
+          "target": "1-westin-em"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "RosaParks",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
