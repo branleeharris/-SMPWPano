26cf4bb2a9f4b17961095fa09653cf56888e055f
branleeharris <branleeharris@gmail.com>
2024-06-24 22:23:12 -0400

    qline update

---
+++ grand-circus-park-q-line/data.js
@@ -0,0 +1,53 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-grand-circus-park--q-line",
+      "name": "Grand Circus Park / Q-Line",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.11978865887495793,
+        "pitch": 0.2981441323060423,
+        "fov": 0.6443975218212802
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.809386423301934,
+          "pitch": -0.03254093893228038,
+          "title": "Emergency Location",
+          "text": "David Whitney Building Lobby<br><br>1 Park Ave, Detroit, MI 48226"
+        }
+      ]
+    }
+  ],
+  "name": "Grand Circus Park / Q-Line",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
