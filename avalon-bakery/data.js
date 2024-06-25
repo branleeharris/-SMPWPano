bbf75f51581e28485fd6bd9f8d16f1c6b1b47095
branleeharris <branleeharris@gmail.com>
2024-06-24 22:44:56 -0400

    avalon upload

---
+++ avalon-bakery/data.js
@@ -0,0 +1,260 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-avalon-primary",
+      "name": "Avalon Primary",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.17132945435490043,
+        "pitch": 0.3184166572756659,
+        "fov": 0.8629344528625367
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6864919698169434,
+          "pitch": 0.15586015044253188,
+          "rotation": 0,
+          "target": "1-avalon-bakery-alternate"
+        },
+        {
+          "yaw": -2.334769064689196,
+          "pitch": 0.006245934554916843,
+          "rotation": 7.0685834705770345,
+          "target": "2-emergency-location---meridian"
+        },
+        {
+          "yaw": 0.4171351059336281,
+          "pitch": 0.3976456511096771,
+          "rotation": 0.7853981633974483,
+          "target": "3-emergency-location---shinola--"
+        },
+        {
+          "yaw": 0.1801188709686592,
+          "pitch": 0.5550122166599465,
+          "rotation": 3.141592653589793,
+          "target": "1-avalon-bakery-alternate"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.4736592676984728,
+          "pitch": 0.0639991180626609,
+          "title": "Avalon Bakery Entrance",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "1-avalon-bakery-alternate",
+      "name": "Avalon Bakery Alternate",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.15002827485663062,
+        "pitch": 0.5357232636817919,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.263713121685827,
+          "pitch": 0.09394335273576537,
+          "rotation": 12.566370614359176,
+          "target": "0-avalon-primary"
+        },
+        {
+          "yaw": -1.7778262598181556,
+          "pitch": 0.048784124314259714,
+          "rotation": 7.0685834705770345,
+          "target": "3-emergency-location---shinola--"
+        },
+        {
+          "yaw": 1.3080586903921976,
+          "pitch": 0.05210493112220149,
+          "rotation": 0.7853981633974483,
+          "target": "2-emergency-location---meridian"
+        },
+        {
+          "yaw": 0.06970190137082355,
+          "pitch": 1.0178288671274238,
+          "rotation": 3.9269908169872414,
+          "target": "0-avalon-primary"
+        },
+        {
+          "yaw": -0.4785932929411558,
+          "pitch": 0.8921609577388772,
+          "rotation": 4.71238898038469,
+          "target": "3-emergency-location---shinola--"
+        },
+        {
+          "yaw": 0.6974232809382634,
+          "pitch": 0.8847156414755695,
+          "rotation": 13.351768777756625,
+          "target": "2-emergency-location---meridian"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.974354727810333,
+          "pitch": 0.01635859690674124,
+          "title": "Avalon Bakery Entrance",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "2-emergency-location---meridian",
+      "name": "Emergency Location - Meridian",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.18842036892436553,
+        "pitch": 0.03590855178207697,
+        "fov": 0.6815270835081485
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8596038684226546,
+          "pitch": 0.07798145059746275,
+          "rotation": 5.497787143782138,
+          "target": "1-avalon-bakery-alternate"
+        },
+        {
+          "yaw": -1.2884861999984913,
+          "pitch": 0.044726378016513024,
+          "rotation": 12.566370614359176,
+          "target": "0-avalon-primary"
+        },
+        {
+          "yaw": -1.0580356315459216,
+          "pitch": -0.287852332926926,
+          "rotation": 3.141592653589793,
+          "target": "3-emergency-location---shinola--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.8361919564928675,
+          "pitch": 0.17631334914181807,
+          "title": "Campus Martius Park",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "3-emergency-location---shinola--",
+      "name": "Emergency Location - Shinola  ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.08581247898400335,
+        "pitch": 0.03479156106388359,
+        "fov": 0.6705709648610689
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.22442819503196,
+          "pitch": 0.044648941871336945,
+          "title": "Shinola Entrance",
+          "text": ""
+        }
+      ]
+    }
+  ],
+  "name": "Avalon Bakery",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
