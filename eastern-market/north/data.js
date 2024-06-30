aaa7603a8e3aba00174befc7a79c94b15d3db53c
branleeharris <branleeharris@gmail.com>
2024-06-29 22:22:57 -0400

    update for Eastern

---
+++ eastern-market/north/data.js
@@ -0,0 +1,273 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-russel--fisherfwy",
+      "name": "Russel / FisherFwy",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.6096426016956293,
+        "pitch": 0.35834728239241365,
+        "fov": 0.8265204845935558
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.11685748411975538,
+          "pitch": 0.23491804261229987,
+          "rotation": 6.283185307179586,
+          "target": "1-market--winder"
+        },
+        {
+          "yaw": -1.2027439441306598,
+          "pitch": 0.07217011228959969,
+          "rotation": 12.566370614359176,
+          "target": "2-germack-coffee"
+        },
+        {
+          "yaw": -0.9182660117853594,
+          "pitch": 0.039475760103371726,
+          "rotation": 5.497787143782138,
+          "target": "3-russell--wilkins"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.651326162694983,
+          "pitch": -0.07999539172865155,
+          "title": "Emergency Location",
+          "text": "Gratiot Central Market"
+        }
+      ]
+    },
+    {
+      "id": "1-market--winder",
+      "name": "Market / Winder",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.257453621363144,
+        "pitch": 0.250681931939436,
+        "fov": 0.9021947879192117
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.20050770472762203,
+          "pitch": 0.12477115841819852,
+          "rotation": 0,
+          "target": "0-russel--fisherfwy"
+        },
+        {
+          "yaw": 1.2998252986158292,
+          "pitch": 0.17144347995472486,
+          "rotation": 0,
+          "target": "2-germack-coffee"
+        },
+        {
+          "yaw": 1.4153125116531875,
+          "pitch": 0.16426627293778928,
+          "rotation": 0.7853981633974483,
+          "target": "3-russell--wilkins"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.529841510703708,
+          "pitch": -0.06358837536453521,
+          "title": "Emergency Location",
+          "text": "Gratiot Central Market"
+        },
+        {
+          "yaw": 2.605969023136713,
+          "pitch": -0.054231005414330014,
+          "title": "Emergency Location",
+          "text": "Parking structure"
+        }
+      ]
+    },
+    {
+      "id": "2-germack-coffee",
+      "name": "Germack Coffee",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.28256259077114443,
+        "pitch": 0.43167062911996723,
+        "fov": 0.922702792761361
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4957760337603947,
+          "pitch": 0.20835833870402354,
+          "rotation": 0,
+          "target": "3-russell--wilkins"
+        },
+        {
+          "yaw": -2.8277157274548195,
+          "pitch": 0.17830301608583454,
+          "rotation": 0,
+          "target": "0-russel--fisherfwy"
+        },
+        {
+          "yaw": -3.0438471524452932,
+          "pitch": 0.18137994548306224,
+          "rotation": 5.497787143782138,
+          "target": "1-market--winder"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.045422696872782,
+          "pitch": -0.09910468576598319,
+          "title": "Emergency Location",
+          "text": "Gratiot Central Market. Located across the freeway."
+        },
+        {
+          "yaw": 1.5004768552791106,
+          "pitch": -0.0988090922093754,
+          "title": "Emergency Location",
+          "text": "Parking structure on the other side of the market sheds."
+        }
+      ]
+    },
+    {
+      "id": "3-russell--wilkins",
+      "name": "Russell / Wilkins",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.04195175577064347,
+        "pitch": 0.3911613619415366,
+        "fov": 0.9031144030988789
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.838333658526796,
+          "pitch": 0.4719914413223094,
+          "rotation": 6.283185307179586,
+          "target": "2-germack-coffee"
+        },
+        {
+          "yaw": -3.1243652136879465,
+          "pitch": 0.2699458229213825,
+          "rotation": 0,
+          "target": "0-russel--fisherfwy"
+        },
+        {
+          "yaw": 2.873962054187195,
+          "pitch": 0.45516990098671606,
+          "rotation": 12.566370614359176,
+          "target": "1-market--winder"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.3615143912414283,
+          "pitch": -0.18987976943547835,
+          "title": "Emergency Location",
+          "text": "Parking structure past these buildings."
+        },
+        {
+          "yaw": 2.92535277328063,
+          "pitch": -0.16171791277466419,
+          "title": "Emergency Location",
+          "text": "Gratiot Central Market (across the freeway)"
+        }
+      ]
+    }
+  ],
+  "name": "Eastern Market",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
