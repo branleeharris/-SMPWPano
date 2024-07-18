e332728beca104e578f0e9dd0845c27a0caf2206
branleeharris <branleeharris@gmail.com>
2024-07-18 16:09:38 -0400

    Oakland 360 Upload

---
+++ oakland-university/data.js
@@ -0,0 +1,204 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-oakland-primary-location",
+      "name": "Oakland Primary Location",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.2012748437102303,
+        "pitch": 0.33461518144329183,
+        "fov": 0.9129771076953225
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.694590488016047,
+          "pitch": 0.10110663985366664,
+          "rotation": 6.283185307179586,
+          "target": "2-oakland-alternate-location"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.6422776360023548,
+          "pitch": 0.28693888648036214,
+          "title": "Rain Location",
+          "text": "If you experience inclimate weather, you can choose to keep the carts up and move them under this awning&nbsp;"
+        },
+        {
+          "yaw": -1.365741333743724,
+          "pitch": 0.08003208141969154,
+          "title": "Emergency Location",
+          "text": "Inside of North Foundation Hall"
+        },
+        {
+          "yaw": 0.5440481622888456,
+          "pitch": 0.04706828574447641,
+          "title": "Emergency Location",
+          "text": "Inside of Oakland Center"
+        }
+      ]
+    },
+    {
+      "id": "1-oakland-rain-location",
+      "name": "Oakland Rain Location",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.01854487255819315,
+        "pitch": 0.32798491786979156,
+        "fov": 0.8839464945624583
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 2.3126601821685178,
+          "pitch": 0.5413648165200673,
+          "title": "Primary Location",
+          "text": "This is the usual location of the carts"
+        },
+        {
+          "yaw": -0.051236380616881405,
+          "pitch": 0.12553777585237036,
+          "title": "Rain Location",
+          "text": "If you experience inclimate weather, you can choose to keep the carts up and move them under this awning&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "2-oakland-alternate-location",
+      "name": "Oakland Alternate Location",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.0743104762782103,
+        "pitch": 0.2504061094815029,
+        "fov": 0.7110524055988134
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.5297013194411218,
+          "pitch": -0.020025796230383364,
+          "title": "Emergency Location",
+          "text": "Inside of Oakland Center"
+        }
+      ]
+    },
+    {
+      "id": "3-oakland-depot",
+      "name": "Oakland Depot",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.017197666186790528,
+        "pitch": 0.31248017784798243,
+        "fov": 0.8053047803217076
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.07879722493824914,
+          "pitch": 0.31731254350871296,
+          "title": "Depot",
+          "text": "Both carts and literature for restocking"
+        }
+      ]
+    }
+  ],
+  "name": "Oakland University",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
